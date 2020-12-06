9d120605bf40bf3ebbe55270ed3f90bea93248ec
Jevin Sidhu <jevin.sidhu@gmail.com>
2020-12-06 01:27:58 -0500

    Add path to server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const request = require("request");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -132,6 +133,10 @@ app.post("/api/world", (req, res) => {
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`
   );
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
